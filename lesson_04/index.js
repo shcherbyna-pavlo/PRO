0ef0a5a8b738b56f80beab37319dc99e76441061
Shcherbyna <sherbina.pavel.g@gmail.com>
2022-10-07 11:14:51 +0300

    +

--- lesson_04/index.js
+++ lesson_04/index.js
@@ -1,13 +1,32 @@
+// let numOrStr = prompt('input number or string');
+
+// console.log(numOrStr)
+
+// if(numOrStr === null) {
+// console.log('вы отменили')
+// } else if( numOrStr.trim() === '' ) {
+// console.log('Empty String');
+// } else if( isNaN( +numOrStr ) ) {
+// console.log(' number is Ba_NaN')
+// } else {
+// console.log('OK!')
+// }
+
 let numOrStr = prompt('input number or string');
 
 console.log(numOrStr)
 
-if(numOrStr === null) {
-console.log('вы отменили')
-} else if( numOrStr.trim() === '' ) {
-console.log('Empty String');
-} else if( isNaN( +numOrStr ) ) {
-console.log(' number is Ba_NaN')
-} else {
-console.log('OK!')
+switch(numOrStr) {
+    case null:
+        console.log('вы отменили');
+        break;
+    case "":
+        console.log('Empty String');
+        break;
+    case isNaN( +numOrStr ):
+        console.log(' number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!');
+        break;
 }
