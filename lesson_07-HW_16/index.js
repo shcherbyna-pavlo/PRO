72d87c7fa278cafd78725edeb2a1140d714eaca4
Shcherbyna <sherbina.pavel.g@gmail.com>
2022-10-20 10:01:07 +0300

    +

---
+++ lesson_07-HW_16/index.js
@@ -0,0 +1,14 @@
+let a = +prompt("Перше число")
+let b = +prompt("степінь")
+
+let c = 1
+let d = a
+function pow (num, degree) {
+    if (c >= degree) return;
+    if (degree === 1) return d;
+    c++
+    d = d*num
+    pow (a, b)
+    return d
+}
+console.log(`${a} в степені ${b} = `, pow (a, b))
