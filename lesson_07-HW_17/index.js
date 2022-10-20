4549a4f01fe06fb699666cc171bc34e1564825a1
Shcherbyna <sherbina.pavel.g@gmail.com>
2022-10-20 14:24:52 +0300

    +

---
+++ lesson_07-HW_17/index.js
@@ -0,0 +1,19 @@
+// ДЗ 17. Функція із замиканням
+
+
+function ff (x) {
+}
+
+
+let a1 = 0;
+function ff1 (y) {
+    a += y
+    return a
+}
+
+
+console.log(ff1 (5))
+console.log(ff1 (7))
+console.log(ff1 (10))
+console.log(ff1 (100))
+console.log(ff1 (11111))
