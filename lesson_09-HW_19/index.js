0bc36ac34bd51471628d2770b6d5d0195819b660
Shcherbyna <sherbina.pavel.g@gmail.com>
2022-11-02 11:21:32 +0200

    +

--- lesson_09-HW_19/index.js
+++ lesson_09-HW_19/index.js
@@ -8,32 +8,48 @@ class Person {
    }
 }
 
-let person = new Person("name", "age");
-console.log(person)
-
-
 class Flat {
    residents = [];
 
-   addResidents(name, age) {
-      this.residents.push(new Person(name, age))
+   addResidents(person) {
+      this.residents.push(person)
    }
 }
 
-let flat = new Flat()
-flat.addResidents("name", "age")
-
-console.log(flat)
-
-
 class House {
    flat = [];
-   addFlat() {
-      this.flat.push(new Flat())
+   
+   addFlat(flat) {
+      if(this.flat.length > 2) {
+         console.log("no")
+      }
+      this.flat.push(flat)
    }
 }
 
 let house = new House()
-house.addFlat()
+
+let flat = new Flat()
+let flat1 = new Flat()
+let flat2 = new Flat()
+
+let residents = new Person("Pavel", 33)
+let residents1 = new Person("Vova", 45)
+let residents2 = new Person("Max", 30)
+let residents3 = new Person("Mark", 19)
+let residents4 = new Person("Tony", 26)
+
+flat.addResidents(residents);
+flat.addResidents(residents1);
+flat.addResidents(residents2);
+flat1.addResidents(residents3);
+flat1.addResidents(residents4);
+flat2.addResidents(residents4);
+console.log(flat)
+
+house.addFlat(flat)
+house.addFlat(flat1)
+house.addFlat(flat2)
+// house.logAllFlat()
+// house.addFlat(flat)
 console.log(house)
-house
