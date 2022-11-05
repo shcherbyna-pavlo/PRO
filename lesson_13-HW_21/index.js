c9191ecf0a76ac274dbc4071af75c331c32dd19a
Shcherbina Pavlo <sherbina.pavel.g@gmail.com>
2022-11-05 18:59:51 +0200

    HW

--- lesson_13-HW_21/index.js
+++ lesson_13-HW_21/index.js
@@ -1,8 +1,16 @@
 const button = document.querySelector('.ghost');
 const input = document.querySelector('.input');
+const wrapper = document.querySelector('.wrapper');
 
-const hideElement = () => {
-    button.style.opacity = '1'
+button.style.opacity = '0';
+
+const hideElement = (ev) => {
+    if(ev.target.closest('input')) {
+        button.style.opacity = '1';
+        return;
+    }
+
+    button.style.opacity = '0';
 }
 
-input.addEventListener('focus', hideElement)
+wrapper.addEventListener('click', hideElement)
