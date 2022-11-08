124e68b2792dba99bbcc26465d7ebdbeeda0d360
Shcherbina Pavlo <sherbina.pavel.g@gmail.com>
2022-11-08 16:19:38 +0200

    +

---
+++ test-slider/index1.js
@@ -0,0 +1,44 @@
+const arrDogs = ['dog1.jpeg', 'dog2.jpeg', 'dog3.jpeg', 'dog4.jpeg', 'dog5.jpeg', 'dog6.jpeg'];
+
+const prev = document.querySelector('.prev')
+const next = document.querySelector('.next')
+
+const image = document.getElementById('image');
+
+function slider() {
+    prev.setAttribute('disabled', "")
+
+    let count = 0;
+    let elArrDogs;
+    
+    function right () {
+        count ++
+    
+        elArrDogs = arrDogs[count];
+        image.src = `./images/${elArrDogs}`
+        image.alt = `${elArrDogs.replace('.jpeg', "")}`
+    
+        if(count === arrDogs.length-1) {
+            next.setAttribute('disabled', "")
+        }
+        prev.removeAttribute('disabled')
+    }
+    
+    function left () {
+        count--
+    
+        elArrDogs = arrDogs[count];
+        image.src = `./images/${elArrDogs}`;
+        image.alt = `${elArrDogs.replace('.jpeg', "")}`;
+        
+        if(count === 0) {
+            prev.setAttribute('disabled', "")
+        }
+        next.removeAttribute('disabled');
+    }
+    
+    prev.addEventListener('click', left)
+    next.addEventListener('click', right)
+}
+
+slider()
