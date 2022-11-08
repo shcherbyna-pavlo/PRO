b7d8b816cbbfdc73ecf9ad0abdd2119e0346a5d0
Shcherbyna <sherbina.pavel.g@gmail.com>
2022-11-08 13:47:47 +0200

    HW

--- lesson_13-HW_24/index.js
+++ lesson_13-HW_24/index.js
@@ -1,33 +1,41 @@
 const arrDogs = ['dog1.jpeg', 'dog2.jpeg', 'dog3.jpeg', 'dog4.jpeg', 'dog5.jpeg', 'dog6.jpeg'];
 const prev = document.querySelector('.prev')
 const next = document.querySelector('.next')
-
 const image = document.getElementById('image');
+prev.setAttribute('disabled', "")
 
-let count = 0;
-
-
-function right () {
-    let elArrDogs = arrDogs[count];
-    image.src = `./images/${elArrDogs}`
-    image.alt = `${elArrDogs.replace('.jpeg', "")}`
+function slider() {
+    let count = 0;
+    let elArrDogs;
     
-    count ++
-    if(count === arrDogs.length) {
-        next.setAttribute('disabled', "")
+    function right () {
+        count ++
+    
+        elArrDogs = arrDogs[count];
+        image.src = `./images/${elArrDogs}`
+        image.alt = `${elArrDogs.replace('.jpeg', "")}`
+    
+        if(count === arrDogs.length-1) {
+            next.setAttribute('disabled', "")
+        }
+        prev.removeAttribute('disabled')
     }
-    console.log(count)
+    
+    function left () {
+        count--
+    
+        elArrDogs = arrDogs[count];
+        image.src = `./images/${elArrDogs}`;
+        image.alt = `${elArrDogs.replace('.jpeg', "")}`;
+        
+        if(count === 0) {
+            prev.setAttribute('disabled', "")
+        }
+        next.removeAttribute('disabled');
+    }
+    
+    prev.addEventListener('click', left)
+    next.addEventListener('click', right)
 }
 
-function left () {
-    
-    if(count === 0) {
-        prev.setAttribute('disabled', "")
-    }
-    count--
-    console.log(count)
-}
-console.log(count)
-prev.addEventListener('click', left)
-next.addEventListener('click', right)
-
+slider()
