608e8d9cf4a83cbe971c020146261a0319a262df
Shcherbyna <sherbina.pavel.g@gmail.com>
2022-11-09 10:12:25 +0200

    test

--- test-slider/index.js
+++ test-slider/index.js
@@ -1,2 +1,52 @@
 const images = document.querySelectorAll('.slider-line img');
-console.log(images)
+const prev = document.querySelector('.prev')
+const next = document.querySelector('.next')
+const sliderLine = document.querySelector('.slider-line')
+const slid = document.querySelector('.slider')
+
+function slider() {
+    prev.setAttribute('disabled', "")
+
+    let count = 0;
+    let width
+
+    function width1 () {
+        width = slid.offsetWidth
+        console.log(width)
+        images.forEach((el) => {
+            el.style.width = width +"px"
+        })
+        sliderLine.style.width = width*images.length + 'px'
+    }
+
+    width1 ()
+
+    function right () {
+        count ++
+    
+        if(count === images.length-1) {
+            next.setAttribute('disabled', "")
+        }
+        prev.removeAttribute('disabled')
+        translate()
+    }
+    
+    function left () {
+        count--
+    
+        if(count === 0) {
+            prev.setAttribute('disabled', "")
+        }
+        next.removeAttribute('disabled');
+        translate()
+    }
+    
+    function translate () {
+        sliderLine.style.transform = 'translate(-' + count * width + 'px)'
+    }
+
+    prev.addEventListener('click', left)
+    next.addEventListener('click', right)
+}
+
+slider()
