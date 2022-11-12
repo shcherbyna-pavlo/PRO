9eb733b920a3264a7d43fee7aaf00981e53f4725
Shcherbina Pavlo <sherbina.pavel.g@gmail.com>
2022-11-12 21:01:26 +0200

    HW

---
+++ lesson_14-HW_25/index.js
@@ -0,0 +1,23 @@
+const reactions = ["👍", "👎", "💚", "💩", "🤣", "🤔", "😍"];
+
+const container = document.querySelector(".container");
+
+const reactionElements = reactions.map((reaction) => {
+  const wrapper = document.createElement("div");
+  const button = document.createElement("button");
+  button.innerText = reaction;
+  const counter = document.createElement("div");
+  counter.classList.add("counter");
+  counter.innerText = 0;
+  wrapper.append(button, counter);
+
+  return wrapper;
+});
+reactionElements.forEach((elem) => container.appendChild(elem));
+
+
+const buttons = Array.from(container.getElementsByTagName('button'));
+
+buttons.forEach((el) => {
+   el.addEventListener('click', () => {el.nextSibling.innerHTML++} );
+});
