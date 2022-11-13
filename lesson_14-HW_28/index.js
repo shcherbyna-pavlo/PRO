1bed04fecfcb2816a04b64d5be0ce2919ce9290e
Shcherbina Pavlo <sherbina.pavel.g@gmail.com>
2022-11-13 16:38:15 +0200

    +

--- lesson_14-HW_28/index.js
+++ lesson_14-HW_28/index.js
@@ -1,22 +1,78 @@
-const arr = [1,2, [1, 2, 3], 3, [5, 6, [8]]];
 
-const r = document.createElement('ul')
+   
+   function f (){
 
+      const listOfLists = [1, 2, [1, 2, 3], 3, 4, [1, 2, [1, 2, 3]], 5];
+      const body = document.querySelector('body')
+      let b = ""
+      
+      body.appendChild(generateList(listOfLists))
 
-   arr.forEach((el) => {
-      // Array.isArray(el) ? r.insertAdjacentHTML('beforeend', `<ul><li>${el}</li></ul>`) : r.insertAdjacentHTML('beforeend', `<li>${el}</li>`)
-      if(Array.isArray(el)) {
-         
-         el.forEach ((el1) => {
-            r.insertAdjacentHTML('beforeend', `<li>${el1}</li>`)
-         })
-
-      } else {
-         r.insertAdjacentHTML('beforeend', `<li>${el}</li>`)
+      function generateList(arr) {
+         const html = document.createElement('ul')
+   
+         arr.forEach((element, index) => {
+            // Array.isArray(element) ? html.insertAdjacentHTML('beforeend', generateList(element).outerHTML) : html.insertAdjacentHTML('beforeend', `<li>${element}</li>`);
+   
+            if(!Array.isArray(element)) {
+               if(b === "") {
+                  html.insertAdjacentHTML('beforeend', `<li>${element}</li>`)
+                  
+               } else if (b !== "") {
+                  html.insertAdjacentHTML('beforeend', `<li>${b}${element}</li>`)
+               }
+   
+               if(Array.isArray(arr[index+1])) {
+                  console.log(b);
+                  b += `${element}.`
+               }
+            }
+            
+            if(Array.isArray(element)) {
+               html.insertAdjacentHTML('beforeend', generateList(element).outerHTML)
+            }
+         });
+         return html
       }
-   })
+      console.log(generateList(listOfLists))
+      
+   }
+   f()
+
+   // generateList(listOfLists)
+
+
+
+
+
+// listOfLists.forEach
+
+
+// arr.forEach((el) => {
+//    if(el === 2){
+//       return html
+//    }
+//    Array.isArray(el) ? html.insertAdjacentHTML('beforeend', a(el).outerHTML) : html.insertAdjacentHTML('beforeend', `<li>${el}</li>`)
+// })
+   // arr.forEach((el) =>{
+   //    // Array.isArray(el) ? r.insertAdjacentHTML('beforeend', `<ul><li>${el}</li></ul>`) : r.insertAdjacentHTML('beforeend', `<li>${el}</li>`)
+   //    if(Array.isArray(el)) {
+         
+   //       el.forEach ((el1) => {
+   //          r.insertAdjacentHTML('beforeend', `<li>${el1}</li>`)
+   //       })
+
+   //    } else {
+   //       r.insertAdjacentHTML('beforeend', `<li>${el}</li>`)
+   //    }
+   // })
 // function e (arr) {
 //    e (arr)
 // }
 
-console.log(r)
+// console.log(r)
+
+// const arr2 = [5, 4, 2, 78, 92, [1, 2]];
+// console.warn(arr2.keys())
+
+// fue
