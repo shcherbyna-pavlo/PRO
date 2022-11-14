053d9b92ac48e39345e1d9a6d19433a9399043c2
Shcherbyna <sherbina.pavel.g@gmail.com>
2022-11-14 07:42:17 +0200

    HW

--- lesson_14-HW27/index.js
+++ lesson_14-HW27/index.js
@@ -1,12 +1,12 @@
 const input = document.querySelector('#url');
 const button = document.querySelector('[type="button"]');
 
-button.addEventListener('click', a);
+button.addEventListener('click', transition);
 
-function a() {
+function transition() {
    if (input.value.includes('http')) {
       location.assign(input.value);
    } else {
       location.assign(`https://${input.value}`);
    }
-}
+};
