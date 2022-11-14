37553575314be6fe2c49cfe2b8ffb8aacc07748f
Shcherbyna <sherbina.pavel.g@gmail.com>
2022-11-14 07:43:51 +0200

    HW

--- lesson_14-HW_28/index.js
+++ lesson_14-HW_28/index.js
@@ -1,28 +1,28 @@
 const listOfLists = [1, 2, [1, 2, 3], 3, 4, [1, 2, [1, 2, 3]], 5];
-const body = document.querySelector('body')
+const body = document.querySelector('body');
 
-body.appendChild(generateList(listOfLists, []))
+body.appendChild(generateList(listOfLists, []));
 
 function generateList(arr, b) {
-   let c = b
+   let c = b;
 
-   const html = document.createElement('ul')
+   const html = document.createElement('ul');
 
    arr.forEach((element, index) => {
       if (!Array.isArray(element)) {
-         html.insertAdjacentHTML('beforeend', `<li>${c.join('')}${element}</li>`)
+         html.insertAdjacentHTML('beforeend', `<li>${c.join('')}${element}</li>`);
       }
 
       if (Array.isArray(element)) {
-         c.push(`${arr[index - 1]}.`)
+         c.push(`${arr[index - 1]}.`);
 
-         html.insertAdjacentHTML('beforeend', generateList(element, c).outerHTML)
+         html.insertAdjacentHTML('beforeend', generateList(element, c).outerHTML);
       }
 
       if (index === arr.length - 1) {
-         c.pop()
+         c.pop();
       }
    });
 
-   return html
-}
+   return html;
+};
