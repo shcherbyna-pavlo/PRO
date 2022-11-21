0db0aa0014feabe288cc5929b2bfffbbcaf9cbad
Shcherbina Pavlo <sherbina.pavel.g@gmail.com>
2022-11-21 20:12:45 +0200

    +

--- lesson_16-HW_30/index.js
+++ lesson_16-HW_30/index.js
@@ -1,48 +1,46 @@
 const registrationForm = document.forms.registrationForm;
-console.log(registrationForm);
 
 const button = document.querySelector('button')
-
 const table = document.querySelector('.table')
 
 const obj = {};
-let p =[]
+const languages = []
 
 function add(ev) {
-
    ev.preventDefault();
-   if(table.childNodes){
 
-      for(let el of registrationForm) {
-         const name = el.name;
-         const type = el.type;
-         const checked = el.checked;
-         const value = el.value;
-         
-         if (name) {
-            if (["radio", "checkbox"].includes(type)) {
-               obj[value] = checked;
-   
-               if(checked === true) {
-                  if(name === 'Language'){
-                     gg(value)
-                  }else {
-                     table.insertAdjacentHTML("beforeend", `<tr><td>${name}</td><td>${value}</td></tr>`)
+   for (let el of registrationForm) {
+      const name = el.name;
+      const type = el.type;
+      const checked = el.checked;
+      const value = el.value;
+
+      if (name) {
+         if (["radio", "checkbox"].includes(type)) {
+            obj[value] = checked;
+
+            if (checked === true) {
+               if (name === "Language") {
+
+                  addLanguages(value);
+               } else {
+                  table.insertAdjacentHTML("beforeend",`<tr><td>${name}</td><td>${value}</td></tr>`);
                }
             }
-          } else {
-               obj[name] = value;
-               table.insertAdjacentHTML("beforeend", `<tr><td>${name}</td><td>${value}</td></tr>`)
-            }
+         } else {
+            obj[name] = value;
+            table.insertAdjacentHTML("beforeend",`<tr><td>${name}</td><td>${value}</td></tr>`);
          }
       }
-     function gg(Languages) {
-      p.push(Languages)
-      }
-   
-      table.insertAdjacentHTML("beforeend", `<tr><td>Languages</td><td>${p.join(', ')}</td></tr>`)
    }
-   registrationForm.style.display = "none"
+
+   function addLanguages(language) {
+      languages.push(language);
+   }
+
+   table.insertAdjacentHTML("beforeend",`<tr><td>Languages</td><td>${languages.join(", ")}</td></tr>`);
+
+   registrationForm.style.display = "none";
 }
 
 button.addEventListener('click', add)
