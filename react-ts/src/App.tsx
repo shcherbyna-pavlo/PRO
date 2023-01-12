69f6ee3e360c2054da3def391c54dc1183370b9e
Shcherbina Pavlo <sherbina.pavel.g@gmail.com>
2023-01-12 13:08:02 +0200

    +

---
+++ react-ts/src/App.tsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header"
+import Main from "./components/Main"
+import Nav from "./components/Nav"
+
+function App() {
+
+  return (
+    <div className="App">
+    <Header />
+    <Nav />
+    <Main />
+    </div>
+  )
+}
+
+export default App
---
+++ react-ts/src/components/Nav.tsx
@@ -0,0 +1,5 @@
+const Nav = () => {
+   return <div className="nav"></div>
+}
+
+export default Nav
