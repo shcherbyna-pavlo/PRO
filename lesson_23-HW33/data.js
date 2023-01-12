5706ad083b20ead21b695a344fd165cb1dced7dd
Shcherbina Pavlo <sherbina.pavel.g@gmail.com>
2023-01-12 10:35:44 +0200

    +

--- lesson_23-HW33/data.js
+++ lesson_23-HW33/data.js
@@ -1,10 +1,10 @@
-export let todos = [
-   { id: 1, name: 'Make pic5re',description: 'ewdwed', price: 304560, isActive: true },
-   { id: 2, name: 'Make pi3ture',description: 'ewdwed', price: 345600, isActive: false },
-   { id: 3, name: 'Make pic4ture',description: 'ewdwed', price: 4500, isActive: true },
+let todos = [
+   { id: 1, name: 'Make pic5re', price: 304560, isActive: true },
+   { id: 2, name: 'Make pi3ture', price: 345600, isActive: true },
+   { id: 3, name: 'Make pic4ture', price: 4500, isActive: false },
 ];
 
-export class MyCustomElement {
+class MyCustomElement {
    className;
    attributeName;
    attributeActive;
--- lesson_23-HW33/product.js
+++ lesson_23-HW33/product.js
@@ -1,14 +1,28 @@
 const productCard = document.getElementsByClassName('product');
 
-console.log(productCard);
-// import {productCard as productCard} from './index.js'
+let {id, name, price, isActive} = history.state
 
-import {MyCustomElement as MyCustomElement} from './todos.js'
-import {g as g1} from './index.js'
-console.log(new MyCustomElement('p'));
+function g (id, name, price, isActive) {
+   let active;
 
+   if(isActive) {
+      active = ''
+   } else {active = 'disabled'};
 
-let d = history.state
-console.log(d.id, d.name, d.price, d.isActive);
+   [...productCard].forEach((el) => {
 
-g1(d.id, d.name, d.price, d.isActive)
+      let cardTitle = new MyCustomElement('div').addClass('product__title');
+      el.insertAdjacentHTML('beforeend', `<${cardTitle.tagName} class = "${cardTitle.className}">${name}</${cardTitle.tagName}>`);
+      
+      let cardImage = new MyCustomElement('div').addClass('product__image');
+      el.insertAdjacentHTML('beforeend', `<${cardImage.tagName} class = "${cardImage.className}"></${cardImage.tagName}>`);
+
+      let cardPrice = new MyCustomElement('div').addClass('product__price');
+      el.insertAdjacentHTML('beforeend', `<${cardPrice.tagName} class = "${cardPrice.className}"= "${id}">${price}</${cardPrice.tagName}>`);
+
+      let cardBtn = new MyCustomElement('button').addClass('product__btn').setAttribute('', `${active}`);
+      el.insertAdjacentHTML('beforeend', `<${cardBtn.tagName} class = "${cardBtn.className}" ${cardBtn.attributeActive}>Add to cart</${cardBtn.tagName}>`);
+   });
+}
+
+g(id, name, price, isActive)
